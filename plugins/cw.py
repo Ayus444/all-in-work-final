22180c74bcf7c94d65991548a1d0f143bec6d17d
Ayush <112577569+Ayus444@users.noreply.github.com>
2025-01-28 22:36:07 +0530

    Update cw.py

--- plugins/cw.py
+++ plugins/cw.py
@@ -54,7 +54,7 @@ BCOV_POLICY = "BCpkADawqM1VmXspFMod94-pT7xDCvmBEYt8U7f0mRB6XnG5huPE7I9qjhDW0qpx3
 bc_url = (f"https://edge.api.brightcove.com/playback/v1/accounts/{ACCOUNT_ID}/videos")
 bc_hdr = {"BCOV-POLICY": BCOV_POLICY}
 
-@bot.on_message(filters.command(["cw"])& ~filters.edited)
+@bot.on_message(filters.command(["cw"]))
 async def account_login(bot: Client, m: Message):
     global cancel
     cancel = False
